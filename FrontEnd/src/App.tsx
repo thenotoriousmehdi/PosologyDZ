27d214452f3feec6492696586509d1a6ea9462d4
Mehdi Mamouni <km_mamouni@esi.dz>
2024-12-08 12:08:29 +0100

    little update

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login"
 import Preparations from "./pages/Preparations";
-
+import DetailsPatient from "./pages/DetailsPatient";
 import Patients from "./pages/Patients";
 //import Patients from "./pages/Patients"
 function App() {
@@ -12,6 +12,8 @@ function App() {
           <Route path="/" element={<Patients/>}/> 
             <Route path="/Login" element={<Login/>}/>  
             <Route path="/Preparations" element={<Preparations/>}/>
+            <Route path="/DetailsPatient" element={<DetailsPatient/>}/>
+            
           </Routes>
     </Router>
   );
